dc9acaf050e76631048a636fa9b52f08608ab55f
Benja-san <beugnet.freelance@gmail.com>
2024-01-18 12:30:37 +0100

    end of demo

--- src/routing.php
+++ src/routing.php
@@ -1,12 +1,16 @@
 <?php
 
-require __DIR__ . '/controllers/recipe-controller.php';
+require __DIR__ . '/controllers/RecipeController.php';
 
 $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
+$recipeController = new RecipeController();
+
 match($urlPath) {
-    '/' => browseRecipes(),
-    '/show' => showRecipe($_GET['id'] ?? null),
-    '/add' => addRecipe(),
+    '/' => $recipeController->browse(),
+    '/show' => $recipeController->show($_GET['id'] ?? null),
+    '/add' => $recipeController->add(),
+    '/edit' => $recipeController->modify($_GET['id'] ?? null),
+    '/delete' => $recipeController->suppress($_GET['id'] ?? null),
     default => header('HTTP/1.1 404 Not Found')
 };
--- src/views/indexRecipe.php
+++ src/views/indexRecipe.php
@@ -5,6 +5,13 @@
         <a href="show?id=<?= $recipe['id'] ?>">
             <?= $recipe['title'] ?>
         </a>
+        <a href="edit?id=<?= $recipe['id'] ?>">
+            <?= "edit " . $recipe['title'] ?>
+        </a>
+        <a href="delete?id=<?= $recipe['id'] ?>">
+            <?= "supprimer " . $recipe['title'] ?>
+        </a>
     </li>
     <?php endforeach; ?>
 </ul>
+<a href="add">Add recipe</a>
